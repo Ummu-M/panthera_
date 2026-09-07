541038de09b3cc8e7a334a0343e0862321d19d66
Ummuayman <moummuayman98@gmail.com>
2026-09-07 11:55:40 +0300

    Make Scout logo icons circular

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,9 +9,9 @@ export const metadata: Metadata = {
   description: 'Kenyatta University Panthera Rover Crew member platform',
   manifest: '/manifest.json',
   icons: {
-    icon: '/icon-192.png',
-    shortcut: '/icon-192.png',
-    apple: '/icon-192.png'
+    icon: '/scout.png',
+    shortcut: '/scout.png',
+    apple: '/scout.png'
   }
 }
 
@@ -19,8 +19,8 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/icon-192.png" type="image/png" />
-        <link rel="apple-touch-icon" href="/icon-192.png" />
+        <link rel="icon" href="/scout.png" type="image/png" />
+        <link rel="apple-touch-icon" href="/scout.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
