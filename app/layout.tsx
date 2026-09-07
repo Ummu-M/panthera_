b152b5ba08402e3a5e23046c01e6f79b21ff6852
Ummuayman <moummuayman98@gmail.com>
2026-09-07 11:39:54 +0300

    Use Scout logo for app icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,9 +9,9 @@ export const metadata: Metadata = {
   description: 'Kenyatta University Panthera Rover Crew member platform',
   manifest: '/manifest.json',
   icons: {
-    icon: '/pantheralogo.png',
-    shortcut: '/pantheralogo.png',
-    apple: '/pantheralogo.png'
+    icon: '/scoutlogo.jpg',
+    shortcut: '/scoutlogo.jpg',
+    apple: '/scoutlogo.jpg'
   }
 }
 
@@ -19,8 +19,8 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/pantheralogo.png" type="image/png" />
-        <link rel="apple-touch-icon" href="/pantheralogo.png" />
+        <link rel="icon" href="/scoutlogo.jpg" type="image/jpeg" />
+        <link rel="apple-touch-icon" href="/scoutlogo.jpg" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
